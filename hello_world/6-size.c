dda5ff9af14f42518c03f684cf0c0213129b7150
“vernfleming” <10568@holbertonstudents.com>
2025-02-17 13:10:07 +0000

    6 fix 803

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
-* description: a program to return data size
-* sizeof: determines size of data type
-* %zu: format specifier
-* main: entry point of program
-* return: exit code
-*/
-
+ * main - Entry point of the program
+ *
+ * Description: Prints the sizes of various data types in bytes.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %zu byte(s)\n", sizeof(float));
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-return (0);
+    return (0);
 }
