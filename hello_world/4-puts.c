e2795e9a5ba8d0cdd4721f1555218a22b579e7bc
“vernfleming” <10568@holbertonstudents.com>
2025-02-17 12:44:24 +0000

    4 fix 10

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void) /* main is the entry point of the program    */
 {
-puts("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
