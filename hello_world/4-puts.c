bf5b82fd68e012ba3e50513cfd86ea00fc9b0851
“vernfleming” <10568@holbertonstudents.com>
2025-02-17 12:42:47 +0000

    4 fix 8 i think

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /* main is the entry point of the program    */
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle"); /* This code prints the string  */
 return (0);
 }
 
