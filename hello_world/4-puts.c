7aa9f705d52d2e2da4a5bb0abd4bb0ca45d9e5ea
“vernfleming” <10568@holbertonstudents.com>
2025-02-17 12:36:21 +0000

    4 fix 5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,4 @@
 #include <stdio.h>
-int main() {puts("\"Programming is like building a multilingual puzzle");return 0;}
+int main(void) {puts("\"Programming is like building a multilingual puzzle");
+  return 0;
+}
