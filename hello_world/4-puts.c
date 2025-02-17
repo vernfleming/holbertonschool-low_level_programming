4e662f037654687ca1170b73daa48db2914af405
“vernfleming” <10568@holbertonstudents.com>
2025-02-17 12:43:35 +0000

    4 fix 9

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void) /* main is the entry point of the program    */
 {
-puts("\"Programming is like building a multilingual puzzle"); /* This code prints the string  */
+puts("\"Programming is like building a multilingual puzzle"); 
 return (0);
 }
 
