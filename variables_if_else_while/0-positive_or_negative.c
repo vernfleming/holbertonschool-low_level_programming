0a9b7674167fbaaea91eaf8e4bbdd911cb9b125b
“vernfleming” <10568@holbertonstudents.com>
2025-02-20 13:54:26 +0000

    0 fix 4

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
-* main: Entry point of the program
-* my_function: assigns logical opertator to value
-* Description: else-if loop for assigning
-* Return: Always 0 (Success)
+* main - Entry point to program
+*
+* Return: Exit code from program
 */
 
 int main(void)
