568b8f69fba3d8a93b7e7b4bb4d4578d030a004e
“vernfleming” <10568@holbertonstudents.com>
2025-02-21 10:57:34 +0000

    9 initial

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point to program
+*
+* Return: Exit code from program
+*/
+
+int main(void)
+{
+int digits;
+for (digits = 0; digits <= 9; digits++)
+{
+putchar(digit + '0');
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return(0);
+}
