5715a132980e0135355ec4e5bae9ef96da7a9add
“vernfleming” <10568@holbertonstudents.com>
2025-02-21 10:26:57 +0000

    6 initial

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point to program
+*
+* Return: Exit code from program
+*/
+
+
+int main(void)
+{
+int digitz;
+for (digitz = 0; digitz <= 9; digitz++)
+{
+putchar(digitz + 0);
+}
+putchar('\n');
+return(0);
+}
+  
