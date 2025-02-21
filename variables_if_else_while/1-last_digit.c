0825f57a05f564d25641dce0b17fbd0cd5cf9828
“vernfleming” <10568@holbertonstudents.com>
2025-02-21 09:45:48 +0000

    1 typo fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d", n, last_digit);
 if (last_digit > 5)
 printf(" and is greater than 5\n");
 else if (last_digit == 0)
-pritnf(" and is 0\n");
+printf(" and is 0\n");
 else
 printf(" and is less than 6 and not 0\n");
 
