30ad84467a903da5f627011afcc4d4d7e257ee37
“vernfleming” <10568@holbertonstudents.com>
2025-02-21 10:42:28 +0000

    7 initial

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point to program
+*
+* Return: Exit code from program
+*/
+
+int main(void)
+{
+char current_letter;
+for (current_letter = 'z'; current_letter >= 'a'; current_letter--)
+{
+putchar(current_letter);
+}
+putchar('\n');
+return (0);
+}
