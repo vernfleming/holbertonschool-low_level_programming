269f0f06d87cf48d4bdaa188d25523a2d80a1593
“vernfleming” <10568@holbertonstudents.com>
2025-02-21 09:56:43 +0000

    2 initial

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point to program
+*
+* Return: Exit code from program
+*/
+
+int main(void)
+{
+char current_letter;
+for (letter = 'a'; letter<= 'z'; letter++)
+(
+putchar(current_letter);
+}
+putchar('\n');
+return (0);
+}
