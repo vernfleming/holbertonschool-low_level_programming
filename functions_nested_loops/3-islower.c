2d60bb6ecc35b8bd5e34d85de8f75d961f487544
“vernfleming” <10568@holbertonstudents.com>
2025-02-24 01:42:06 +0000

    3 initial and update header

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower identifies if a character is lower case
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+)
