67c6f524434c62752a0070e77349fa2949a063d6
“vernfleming” <10568@holbertonstudents.com>
2025-02-24 02:30:35 +0000

    8 fix 2

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ for (minute = 0; minute < 60; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
-_putchar(':');               
+_putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar('\n');
