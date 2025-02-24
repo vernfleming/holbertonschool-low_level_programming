5589b29a20d4568207a19cacdb961bbf38c24967
“vernfleming” <10568@holbertonstudents.com>
2025-02-24 02:02:40 +0000

    6 initial

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - gives absolute value of int.
+ * @n: the integer to check.
+ *
+ * Return: absolute value of n.
+ */
+
+
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
