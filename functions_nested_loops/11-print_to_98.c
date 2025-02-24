2d6812a5a847b081f5d2b1056df4b5854c3e5089
“vernfleming” <10568@holbertonstudents.com>
2025-02-24 02:53:41 +0000

    11 fix 1

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,8 @@ void print_number(int n)
 {
 if (n < 0)
 {
-_putchar('-';
-n = -n;
+  _putchar('-');
+  n = -n;
 }
 if (n / 10)
 print_number(n / 10);
@@ -24,7 +24,7 @@ _putchar((n % 10) + '0');
   
 void print_to_98(int n)
 {
-while (n! = 98)
+while (n != 98)
 {
 print_number(n);
 _putchar(',');
