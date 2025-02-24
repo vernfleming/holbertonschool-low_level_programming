24d6912f9f2723a6f48eee09f64e00590e97f961
“vernfleming” <10568@holbertonstudents.com>
2025-02-24 02:43:20 +0000

    10 initial

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sumss two numbers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+  return (a + b);
+}
