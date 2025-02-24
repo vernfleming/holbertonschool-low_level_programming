5e6d4f5992edb23a7e8cb41ce3fb27d88b192cd6
“vernfleming” <10568@holbertonstudents.com>
2025-02-24 00:29:34 +0000

    all files and solutions to 0, 1, 2

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void putchar_jb(char c) {
+putchar(c);
+}
+
+void letters(void) {
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++) {
+}
+_putchar('\n');
+}
+
+int main(void) {
+letters();
+return(0);
+}
+
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**                                                                                                                              
+ * main - check the code                                                                                                         
+ *                                                                                                                               
+ * Return: Always 0.                                                                                                             
+ */
+
+void putchar_jb(char c) {
+putchar(c);
+}
+
+void letters(void) {
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++) {
+}
+_putchar('\n');
+}
+
+void letters_10(void) {
+int i;
+for (i = 0; i <10; i++) {
+letters():
+}
+}
+
+int main(void) {
+letters_10();
+return(0);
+}
