faab9820cb0fa1b62c35e6e409a695f0bd526780
“vernfleming” <10568@holbertonstudents.com>
2025-03-01 10:48:13 +0000

    header update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
