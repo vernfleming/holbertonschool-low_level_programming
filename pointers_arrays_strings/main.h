d5c722e3f87ac0f0930b1edb4f111ae342fba06b
“vernfleming” <10568@holbertonstudents.com>
2025-03-01 10:20:10 +0000

    initial 2

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+
+#endif /* MAIN_H */
