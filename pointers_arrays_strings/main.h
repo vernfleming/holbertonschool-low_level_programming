b8d42dc0b03f0ad91cbf3e82b6280f524d552c1a
“vernfleming” <10568@holbertonstudents.com>
2025-03-02 09:54:17 +0000

    initial set-up project 14

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Project 13  */
+
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
@@ -14,5 +16,13 @@ void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 
+/* Project 14  */
+
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
