cc7984554a51bdfba18630e095260fd9929729db
“vernfleming” <10568@holbertonstudents.com>
2025-03-03 23:54:29 +0000

    initial

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,15 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 
+/* Project 15  */
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
 #endif /* MAIN_H */
