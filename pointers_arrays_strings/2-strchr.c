36a7f4e76842b3d82da13a7f890051f971b7baa9
“vernfleming” <10568@holbertonstudents.com>
2025-03-04 00:36:56 +0000

    2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates character in string
+ *  @s: pointer
+ *  @c: character
+ *  
+ *  return: pointer to first c or null if not there
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+return (s);
+S++;
+}
+if (c == '\0')
+return (s);
+return (0);
+}
