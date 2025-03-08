0f3039cb53d81624313965914998d9a101cfcae5
“vernfleming” <10568@holbertonstudents.com>
2025-03-08 09:12:14 +0000

    7 and 8

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: shows chesspieces on board in alpha form
+ */
+
+void print_chessboard(char (*a)[8]) {
+int f, g;
+for (f = 0; f < 8; f++)
+{
+for (g = 0; g< 8; g++)
+{
+putchar(a[f][g]);
+}
+putchar('\n');
+}
+}
