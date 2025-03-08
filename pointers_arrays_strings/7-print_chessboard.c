5c1a0a3bad739eb9d5243b9bc36291ac4f01b04f
“vernfleming” <10568@holbertonstudents.com>
2025-03-08 09:13:30 +0000

    7 fix 1

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,11 +6,12 @@
  * @a: shows chesspieces on board in alpha form
  */
 
-void print_chessboard(char (*a)[8]) {
+void print_chessboard(char (*a)[8])
+{
 int f, g;
 for (f = 0; f < 8; f++)
 {
-for (g = 0; g< 8; g++)
+for (g = 0; g < 8; g++)
 {
 putchar(a[f][g]);
 }
