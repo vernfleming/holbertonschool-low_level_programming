be7cfb8fc89d81a463bde0c5d6ac3ff8eb3620d3
“vernfleming” <10568@holbertonstudents.com>
2025-03-08 05:35:14 +0000

    4 fix 1

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,25 +5,27 @@
  * @s: the string to be searched
  * @accept: the set of characters to look for
  * 
- * Description: this function searches the string 's' for the first occurrence 
- * of any character that exists in the string 'accept'. It returns a pointer to 
+ * Description: this function searches the string 's' for the first occurrence
+ * of any character that exists in the string 'accept'. It returns a pointer to
  * the first matching character in 's'. If no match is found, it returns NULL.
  * 
  * Return: a pointer to the first occurrence of any character from 'accept'
  * or NULL if no match
- * 
  */
 
 char *_strpbrk(char *s, char *accept) {
-  while (*s) {
-    const char *a = accept;
-    while (*a) {
-      if (*s == *a) {
-	return (char *)s;
-      }
-      a++;
-    }
-    s++;
-  }
-  return NULL;
+while (*s)
+{
+const char *a = accept;
+while (*a)
+{
+if (*s == *a)
+{
+return (char *)s;
+}
+a++;
+}
+s++;
+}
+return NULL;
 }
