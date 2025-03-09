f46fe707f1ce031c6ec7ec22a33f691c80ec27c8
“vernfleming” <10568@holbertonstudents.com>
2025-03-09 09:06:23 +0000

    0, 1 fixes 2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints current program name
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
 (void)argv;
-_printf("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 return 0;
 }
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,15 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: Arg count
+ * @argv: Arg vector
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
 (void)argv;
-_printf("%d\n", argc - 1);
+printf("%d\n", argc - 1);
 return 0;
 }
