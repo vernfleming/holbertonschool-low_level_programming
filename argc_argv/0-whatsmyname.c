a8e16b420d4f93aae20b59f6389abe72b77dcfea
“vernfleming” <10568@holbertonstudents.com>
2025-03-09 09:03:15 +0000

    0, 1 fixes 1

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char *argv[])
 {
+(void)argv;
 _printf("%s\n", argv[0]);
 return 0;
 }
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char *argv[])
 {
+(void)argv;
 _printf("%d\n", argc - 1);
 return 0;
 }
