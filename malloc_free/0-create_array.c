b7f0592d12887a1bacc9206ddc1eaf8f66cc414d
“vernfleming” <10568@holbertonstudents.com>
2025-03-10 23:24:20 +0000

    0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+
+char *create_array(unsigned int size, char c) {
+
+  char *arr;
+  unsigned int i;
+
+  if (size == 0)
+    return NULL;
+
+  arr = (char *)malloc(size * sizeof(char));
+  if (arr == NULL)
+    return NULL;
+
+  for (i = 0; i < size; i++)
+    arr[i] = c;
+
+  return arr;
+}
