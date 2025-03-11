38a37f443bddfde0b5c0ca4b97838ecdfbbad747
“vernfleming” <10568@holbertonstudents.com>
2025-03-11 00:40:52 +0000

    malloc 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+char *str_concat(char *s1, char *s2) {
+
+  char *concat;
+  unsigned int length1 = 0, length2 = 0, i, j;
+
+    
+    if (s1 != NULL)
+      {
+	while (s1[length1] != '\0')
+	  length1++;
+      }
+
+        if (s2 != NULL)
+      {
+	while (s2[length2] != '\0')
+          length2++;
+      }
+
+	concat = (char *)malloc(legnth1 + length2 + 1) * sizeof(char));
+if (concat == NULL)
+  return (NULL);
+
+for (i = 0; i < length1; i++)
+  concat[i] = s1[i];
+
+for (j = 0; j < length2; j++, i++)
+  concat[i] = s2[j];
+
+concat[i] = '\0';
+return (concat);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,44 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Project 13  */
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+/* Project 14  */
+
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+
+/* Project 15  */
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+/* Malloc */
+
+char *str_concat(char *s1, char *s2);
+
+#endif /* MAIN_H */
