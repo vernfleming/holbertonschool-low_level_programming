98a684822bdeaf6d57f8340d46cbfac3c50df211
“vernfleming” <10568@holbertonstudents.com>
2025-03-11 14:10:26 +0000

    fixes

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c) {
 char *arr;
 unsigned int i;
 if (size == 0)
-return NULL;
+return (NULL);
 arr = (char *)malloc(size * sizeof(char));
 if (arr == NULL)
-return NULL;
+return (NULL);
 for (i = 0; i < size; i++)
 arr[i] = c;
-return arr;
+return (arr);
 }
