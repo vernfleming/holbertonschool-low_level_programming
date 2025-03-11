1634a37291d4a113a009687b45c5406dbf375a94
“vernfleming” <10568@holbertonstudents.com>
2025-03-11 14:07:44 +0000

    1, 2, 3

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,20 +1,22 @@
 #include <stdlib.h>
-#include <time.h>
+
+/**
+ * create_array - allocates memory for an array and initializes
+ * @size: the number of elements in the array
+ * @c: the character to initialize each element of the array
+ *
+ * Return: A pointer to the allocated memory, or NULL if 0
+ */
 
 char *create_array(unsigned int size, char c) {
-
-  char *arr;
-  unsigned int i;
-
-  if (size == 0)
-    return NULL;
-
-  arr = (char *)malloc(size * sizeof(char));
-  if (arr == NULL)
-    return NULL;
-
-  for (i = 0; i < size; i++)
-    arr[i] = c;
-
-  return arr;
+char *arr;
+unsigned int i;
+if (size == 0)
+return NULL;
+arr = (char *)malloc(size * sizeof(char));
+if (arr == NULL)
+return NULL;
+for (i = 0; i < size; i++)
+arr[i] = c;
+return arr;
 }
