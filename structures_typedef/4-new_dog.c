48edf6f19a4caa0d934961ef361ac74a2b72fea3
“vernfleming” <10568@holbertonstudents.com>
2025-03-16 07:37:37 +0000

    4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,51 +3,79 @@
 #include "dog.h"
 
 
-int string_length(char *str) {
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    return len;
+/**
+ * string_length - finds us the length of the string
+ *
+ * @s: pointer to the start of a string
+ * Description: as above
+ * Return: len
+ */
+
+int string_length(char *str)
+{
+int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+return len;
 }
 
-dog_t *new_dog(char *name, float age, char *owner) {
-  int i;
-  dog_t *d = malloc(sizeof(dog_t));
-    if (d == NULL) {
-        return NULL;
-    }
+/**
+ * new_dog - lets us create a brand new doggo
+ *
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: owner of the new dog
+ *
+ * Description: as above
+ *
+ * Return: pointer to the data structure of the new dog
+ */
 
-    if (name != NULL) {
-        d->name = malloc(string_length(name) + 1);
-        if (d->name == NULL) {
-            free(d);
-            return NULL;
-        }
-        for (i = 0; name[i] != '\0'; i++) {
-            d->name[i] = name[i];
-        }
-        d->name[string_length(name)] = '\0';
-    } else {
-        d->name = NULL;
-    }
-
-    if (owner != NULL) {
-        d->owner = malloc(string_length(owner) + 1);
-        if (d->owner == NULL) {
-            free(d->name);
-            free(d);
-            return NULL;
-        }
-        for (i = 0; owner[i] != '\0'; i++) {
-            d->owner[i] = owner[i];
-        }
-        d->owner[string_length(owner)] = '\0';
-    } else {
-        d->owner = NULL;
-    }
-
-    d->age = age;
-
-    return d;
+dog_t *new_dog(char *name, float age, char *owner)
+{
+int i;
+dog_t *d = malloc(sizeof(dog_t));
+if (d == NULL)
+{
+return (NULL);
+}
+if (name != NULL)
+{
+d->name = malloc(string_length(name) + 1);
+if (d->name == NULL)
+{
+free(d);
+return (NULL);
+}
+for (i = 0; name[i] != '\0'; i++)
+{
+d->name[i] = name[i];
+}
+d->name[string_length(name)] = '\0';
+} else
+{
+d->name = NULL;
+}
+if (owner != NULL)
+{
+d->owner = malloc(string_length(owner) + 1);
+if (d->owner == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+for (i = 0; owner[i] != '\0'; i++)
+{
+d->owner[i] = owner[i];
+}
+d->owner[string_length(owner)] = '\0';
+} else
+{
+d->owner = NULL;
+}
+d->age = age;
+return (d);
 }
