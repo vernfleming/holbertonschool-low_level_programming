e562b054b1c2dd92d7b00cc78030a46bd2f3d8f2
“vernfleming” <10568@holbertonstudents.com>
2025-03-16 06:45:20 +0000

    doggos

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * init_dog - creates  the dog data  structure
+ *
+ * @d: location  of the data  structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: as above
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints the dog data structure
+ * @d: location of the dog data structure
+ * Description: as above
+ *
+ * Return: void
+ */
+
+struct dog {
+char *name;
+float age;
+char *owner;
+};
+
+void print_dog(struct dog *d) {
+if (d == NULL)
+{
+return;
+}
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+} else
+{
+printf("Name: %s\n", d->name);
+}
+printf("Age: %.6f\n", d->age);
+if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+} else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
+
+
