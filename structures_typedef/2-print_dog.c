c18ef8f11e15534ae201266585bef5d54b47a9a1
“vernfleming” <10568@holbertonstudents.com>
2025-03-16 06:58:26 +0000

    fixing 2 betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,8 @@
  * Return: void
  */
 
-void print_dog(struct dog *d) {
+void print_dog(struct dog *d)
+{
 if (d == NULL)
 {
 return;
@@ -18,7 +19,8 @@ return;
 if (d->name == NULL)
 {
 printf("Name: (nil)\n");
-} else
+}
+else
 {
 printf("Name: %s\n", d->name);
 }
@@ -26,7 +28,8 @@ printf("Age: %.6f\n", d->age);
 if (d->owner == NULL)
 {
 printf("Owner: (nil)\n");
-} else
+}
+else
 {
 printf("Owner: %s\n", d->owner);
 }
