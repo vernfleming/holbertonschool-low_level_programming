0784393d44c119ba36b82a6cd5aed61bcd3de999
“vernfleming” <10568@holbertonstudents.com>
2025-03-16 07:27:54 +0000

    4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>  /* For malloc and free */
+#include <stdlib.h>
+#include "dog.h"
 
-/* Define the dog structure */
-typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
 
-/* Function to calculate the length of a string manually */
 int string_length(char *str) {
   
     int len = 0;
@@ -18,19 +12,19 @@ int string_length(char *str) {
     return len;
 }
 
-/* Function to create a new dog */
+
 dog_t *new_dog(char *name, float age, char *owner) {
-    /* Allocate memory for the new dog structure */
+
   int i;
     dog_t *d = malloc(sizeof(dog_t));
-    if (d == NULL) {  /* Check if malloc failed */
+    if (d == NULL) {
         return NULL;
     }
 
     /* Allocate memory for the name string and copy the content manually */
     if (name != NULL) {
-        d->name = malloc(string_length(name) + 1);  /* +1 for the null terminator */
-        if (d->name == NULL) {  /* Check if malloc failed */
+        d->name = malloc(string_length(name) + 1);
+        if (d->name == NULL) {
             free(d);
             return NULL;
         }
@@ -38,9 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner) {
         for (i = 0; name[i] != '\0'; i++) {
             d->name[i] = name[i];
         }
-        d->name[string_length(name)] = '\0';  /* Null terminate the string */
+        d->name[string_length(name)] = '\0';
     } else {
-        d->name = NULL;  /* If name is NULL, set it to NULL */
+        d->name = NULL;
     }
-
-    /* Allocate memory for the owner string and copy the content manually
