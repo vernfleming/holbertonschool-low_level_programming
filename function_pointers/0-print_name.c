1ce92c9a3dbdd5abe6cf5dd66addd0a4a5ed4ae1
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 12:12:30 +0000

    0 fix

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,15 +3,11 @@
 
 /**
  * print_name - function that prints a name
- *
  * @name: the name string 
  * @f: function pointer to alter print name
- *
  * Description: print_name takes a name as an input
- *
  * Return: void
  */
-
 
 void print_name(char *name, void (*f)(char *))
 {
