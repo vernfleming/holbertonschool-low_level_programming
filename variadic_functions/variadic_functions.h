c94e515a61ffdff0c586baa828cf31c12867b9c5
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 12:40:01 +0000

    various fixes

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,15 +6,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
-typedef struct print_func{
+typedef struct print_func {
 char *pf;
-char (*f)(char *s);
-};
+void (*f)(char *s);
+} print_func;
 
 void print_char(char *s);
 void print_int(char *s);
 void print_float(char *s);
 void print_str(char *s);
-void ((*get_print_func_fp)(char *s))(char *s);
+
+void (*get_print_func_fp)(char *s);
 
 #endif
+
