ae610fe643f8b68a065ad76af068249f457523c5
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 13:32:50 +0000

    fix all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,21 +8,17 @@
  *
  * Return: The sum of all the parameters, or 0 if n == 0
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int sum = 0;
-    va_list args;
-
-    if (n == 0)
-        return (0);
-
-    va_start(args, n);
-
-    for (i = 0; i < n; i++)
-        sum += va_arg(args, int);
-
-    va_end(args);
-
-    return (sum);
+unsigned int i;
+int sum = 0;
+va_list args;
+if (n == 0)
+return (0);
+va_start(args, n);
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+va_end(args);
+return (sum);
 }
