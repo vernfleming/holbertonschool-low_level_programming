aa1faa789cb04a8f9babe904ffb7ab9682021463
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 12:36:57 +0000

    0 fix

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,27 +3,21 @@
 
 /**
  * sum_them_all - simply sums the arguments
- *
  * @n: number of arguments
- *
  * Description: as above
- *
  * Return: the sum of values
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-va_list num;
+va_list args;
 unsigned int i = 0;
-int total = 0;
+int sum = 0;
 if (n == 0)
 return (0);
-va_start(num, n);
-while (i < n)
-{
-total = total + va_arg(num, int);
-i++;
-}
-va_end(num);
-return (total);
+va_start(args, n);
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+va_end(args);
+return (sum);
 }
