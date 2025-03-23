1b30b5e458187bacaea8b57391aeef4cb8ed39fd
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 14:16:36 +0000

    fix 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,17 +3,20 @@
 
 #include <stdarg.h>
 
-typedef struct print_func {
-    char pf;              /* Format character */
-    void (*f)(va_list args); /* Function pointer to handle the argument */
-} print_funct;
-
-/* Function prototypes */
-void print_all(const char * const format, ...);
-void print_char(va_list args);
-void print_int(va_list args);
-void print_float(va_list args);
-void print_str(va_list args);
 int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
-#endif /* VARIADIC_FUNCTIONS_H */
+typedef struct print_func{
+	char *pf;
+	char (*f)(char *s);
+};
+
+void print_char(char *s);
+void print_int(char *s);
+void print_float(char *s);
+void print_str(char *s);
+void ((*get_print_func_fp)(char *s))(char *s);
+
+#endif
