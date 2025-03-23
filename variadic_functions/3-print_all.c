ec66eb6c9bb7faa34401af637d1efe5746391c25
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 13:45:45 +0000

    fix all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,55 +1,136 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdlib.h>
 
 /**
- * print_all - Prints anything based on the format string
- * @format: A list of types of arguments passed to the function
+ * print_all - Prints the arguments based on the chars in the format
  *
- * Return: Nothing
+ * @format: A string/set of chars which specify what data types to print
+ *
+ * Description: Prints the arguments based on the chars in the format string
+ *
+ * Return: void
  */
 
-void print_all(const char * const format, ...) {
-    unsigned int i = 0;
-    va_list args;
-    char *str;
-    char c;
-    int d;
-    float f;
-    int printed = 0;  /* To track if we need to print the separator */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char (*get_print_func_fp)(char *s);
+	int i = 0, j = 0;
 
-    va_start(args, format);
-    while (format && format[i] != '\0') {
-        if (printed)  /* Print separator only if it's not the first element */
-            printf(", ");
+	va_start(args, format);
 
-        switch (format[i]) {
-            case 'c':
-                c = va_arg(args, int);  /* char is promoted to int */
-                printf("%c", c);
-                break;
-            case 'i':
-                d = va_arg(args, int);
-                printf("%d", d);
-                break;
-            case 'f':
-                f = va_arg(args, double);  /* float is promoted to double */
-                printf("%f", f);
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str)
-                    printf("%s", str);
-                else
-                    printf("(nil)");
-                break;
-            default:
-                i++;
-                continue;
-        }
-        printed = 1;  /* Set printed to 1 once a value is printed */
-        i++;
-    }
-    va_end(args);
-    printf("\n");
+	while (i < 4)
+	{
+		j = 0;
+
+		while (pfs[j].pf != '\0')
+		{
+			if (format[i] == pfs[j].pf
+				get_print_func_fp[j].f(va_args(args, char *);
+			j++;
+		}
+	i++;
+
+	va_end(args);
+	}
+}
+
+/**
+ * get_print_func_fp - Function pointer to the right print function
+ *
+ * @s: String from format to compare
+ *
+ * Description: Function pointer to the right print function
+ *
+ * Return: void
+ */
+
+char (*get_print_func_fp)(char *s)
+{
+	print_funct pfs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (pfs[i].pf != NULL)
+	{
+		if (*s == *pfs[i].pf)
+			return (pfs[i].f);
+		i++;
+	}
+	return (pfs[i].f);
+}
+
+/**
+ * print_char - Prints a character
+ *
+ * @c: Character
+ *
+ * Description: Prints a character
+ *
+ * Return: void
+ */
+
+void print_char(char c);
+{
+	_putchar(c);
+}
+
+/**
+ * print_int - Prints an integer
+ *
+ * @s: String of int to print
+ *
+ * Description: Prints an integer
+ *
+ * Return: void
+ */
+
+void print_int(char *s)
+{
+	int n;
+
+	n = atoi(s);
+	printf("%d", n);
+}
+
+/**
+ * print_float - Prints a float
+ *
+ * @s: String of float to print
+ *
+ * Description: Prints a float
+ *
+ * Return: void
+ */
+
+void print_float(char *s)
+{
+	float n;
+
+	n = atof(s);
+	printf("%f", n);
+}
+
+/**
+ * print_str - Prints a string
+ *
+ * @s: String to print
+ *
+ * Description: Prints a string
+ *
+ * Return: void
+ */
+
+void print_str(char *s)
+{
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,5 +8,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+char (*get_print_func_fp(char *s))(char *s);
+void print_char(char c);
+void print_int(char *s);
+void print_float(char *s);
+void print_str(char *s);
+
+typedef struct print_func {
+    char *pf;
+    void (*f)(char *s);
+} print_funct;
+
 
 #endif
