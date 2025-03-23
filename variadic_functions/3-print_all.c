507fb2bde1acd70a3095ff6b49803edd64d90e62
“vernfleming” <10568@holbertonstudents.com>
2025-03-23 12:52:11 +0000

    various fixes

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,123 +1,111 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_all - prints arguments
- *
- * @format: format
- * Description: as above
- *
- * Return: void
+ * print_char - Prints a char
+ * @args: Argument list
+ */
+void print_char(va_list args)
+{
+    _putchar(va_arg(args, int));
+}
+
+/**
+ * print_int - Prints an integer
+ * @args: Argument list
+ */
+void print_int(va_list args)
+{
+    int n = va_arg(args, int);
+    int div = 1, temp;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+    temp = n;
+    while (temp / 10)
+    {
+        div *= 10;
+        temp /= 10;
+    }
+    while (div)
+    {
+        _putchar((n / div) + '0');
+        n %= div;
+        div /= 10;
+    }
+}
+
+/**
+ * print_float - Prints a float (only 6 decimal places)
+ * @args: Argument list
+ */
+void print_float(va_list args)
+{
+    double f = va_arg(args, double);
+    int int_part = (int)f;
+    int dec_part, i;
+
+print_int((va_list) &int_part);
+_putchar('.');    
+f -= int_part;
+for (i = 0; i < 6; i++)
+f *= 10;
+dec_part = (int)f;
+print_int((va_list) &dec_part);
+}
+
+/**
+ * print_str - Prints a string
+ * @args: Argument list
+ */
+void print_str(va_list args)
+{
+char *s = va_arg(args, char *);
+if (!s)
+s = "(nil)";
+while (*s)
+_putchar(*s++);
+}
+
+/**
+ * print_all - Prints any type of argument
+ * @format: Format string specifying argument types
  */
 
 void print_all(const char * const format, ...)
 {
 va_list args;
-char (*get_print_func_fp)(char *s);
-int i = 0, j = 0;
+int i = 0, j;
+char *sep = "";
+struct print_func {
+char spec;
+void (*func)(va_list);
+} pfs[] = 
+{
+{'c', print_char}, {'i', print_int},
+{'f', print_float}, {'s', print_str}, {'\0', NULL}
+};
 va_start(args, format);
-while (i < 4)
+while (format && format[i])
 {
 j = 0;
-while (pfs[j].pf != '\0')
+while (pfs[j].spec)
 {
-if (format[i] == pfs[j].pf
-get_print_func_fp[j].f(va_args(args, char *);
+if (format[i] == pfs[j].spec)
+{
+while (*sep)
+_putchar(*sep++);
+pfs[j].func(args);
+sep = ", ";
+break;
+}
 j++;
 }
 i++;
+}
 va_end(args);
-}
-}
-
-/**
- * get_print_func_fp - function pointer
- *
- * @s: string
- * Description: as above
- *
- * Return: void
- */
-
-char (*get_print_func_fp)(char *s)
-{
-print_funct pfs[] = {
-{"c", print_char},
-{"i", print_int},
-{"f", print_float},
-{"s", print_str},
-{NULL, NULL}
-};
-int i = 0;
-while (pfs[i].pf != NULL)
-{
-if (*s == *pfs[i].pf)
-return (pfs[i].f);
-i++;
-}
-return (pfs[i].f);
-}
-
-/**
- * print_char - prints a char
- *
- * @c: char
- * Description: as above
- *
- * Return: void
- */
-
-void print_char(char c);
-{
-_putchar(c);
-}
-
-/**
- * print_int - simply prints an integer
- *
- * @s: string of ints to print
- * Description: see above
- *
- * Return: void
- */
-
-void print_int(char *s)
-{
-int n;
-n = atoi(s);
-printf("%d", n);
-}
-
-/**
- * print_float - prints a float
- *
- * @s: string of floats to print
- * Description: as above
- *
- * Return: void
- */
-
-void print_float(char *s)
-{
-float n;
-n = atof(s);
-printf("%f", n);
-}
-
-/**
- * print_str - prints a string
- *
- * @s: string to be printed
- * Description: as above
- *
- * Return: void
- */
-
-void print_str(char *s)
-{
-if (s == NULL)
-s = "(nil)";
-printf("%s", s);
+_putchar('\n');
 }
