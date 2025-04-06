b972cd672990b0600d66cd1d6d5d0b403475d80b
“vernfleming” <10568@holbertonstudents.com>
2025-04-06 22:44:16 +1000

    3 fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,6 +31,14 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	r_bytes = read(fd_from, buffer, BUF_SIZE);
+	if (r_bytes == -1)
+	{
+		close(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -39,16 +47,8 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) != 0)
+	while (r_bytes > 0)
 	{
-		if (r_bytes == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
@@ -56,6 +56,15 @@ int main(int argc, char *argv[])
 			close(fd_to);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
+		}
+
+		r_bytes = read(fd_from, buffer, BUF_SIZE);
+		if (r_bytes == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
 	}
 
