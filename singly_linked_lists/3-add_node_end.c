cd0bf16a593ac987f599e78fcdf1ad462ccf914d
“vernfleming” <10568@holbertonstudents.com>
2025-04-06 20:45:13 +1000

    3 fix

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	list_t *curr_node;
 	int str_len = 0;
-	
+
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
