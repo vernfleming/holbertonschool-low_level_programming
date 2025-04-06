1faa4ad3ab89b31b7f7792831c73d45d6260467b
“vernfleming” <10568@holbertonstudents.com>
2025-04-06 20:42:11 +1000

    header fix

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,6 +23,7 @@ struct list_s *next;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
