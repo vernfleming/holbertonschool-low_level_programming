5c7aa1574c8935410af38494a245acb2fbbdea51
“vernfleming” <10568@holbertonstudents.com>
2025-04-06 19:38:46 +1000

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,20 +11,15 @@
 
 size_t print_list(const list_t *h)
 {
-  size_t count = 0;
-
-  while (h != NULL)
-    {
-
-      if (h->str == NULL)
-	printf("[0] (nil)\n");
-      else
-	printf("[%u] %s\n", h->len, h->str);
-
-       h = h->next;
-        count++;
-
-    }
-
-  return (count);
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
 }
