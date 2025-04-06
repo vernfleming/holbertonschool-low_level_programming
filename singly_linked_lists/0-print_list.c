c1ca7520b86b8bd899bd726495f90f9adbe2c4b4
“vernfleming” <10568@holbertonstudents.com>
2025-04-06 20:09:16 +1000

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,14 +1,11 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
-
 /**
  * print_list - prints every element of a list
  * @h: pointer to the start of the list
- *
  * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
